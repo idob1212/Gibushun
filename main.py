e94390eb8ca60041131ff29bdaadb5d9a55e305d
repl.it user <replituser@example.com>
2022-12-21 00:05:17 +0200

    v1

--- main.py
+++ main.py
@@ -968,3 +968,6 @@ def downloadb():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
